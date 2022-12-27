42c8bf7e823c663c4b291a2c891e6f24addd9992
CappelloNicola <cappellonicola04@gmail.com>
2022-12-27 13:57:24 +0100

    Url problem resolved
    Url problem resolved by adding disableOnDeploy.js file

--- scripts/disableOnDeploy.js
+++ scripts/disableOnDeploy.js
@@ -7,6 +7,26 @@
     when deploying "CappelloNicola.github.io" must be avoided
 
     i could make a script where, for every link, if the link starts with "/" i add "/CappelloNicola.github.io/"
-    I disable the script when deploying.
+    I disable the script when deploying by commenting the function below
 */
 
+/*
+$(document).ready(function(){
+    var allLinks = $('a');
+
+    for (var i = 0; i<allLinks.length; i++){
+        var element = allLinks[i];
+        var href = element.href;
+        var indexOfLocalhost = href.indexOf("localhost");
+        var newHref;
+
+        if(indexOfLocalhost!=-1){
+            var indexOfSlash = href.indexOf("/", indexOfLocalhost);
+            newHref = href.substring(0, indexOfSlash+1) + 'CappelloNicola.github.io/' + href.substring(indexOfSlash + 1 );
+            console.log(newHref);
+            element.href = newHref;
+        }
+    }
+
+});
+*/
