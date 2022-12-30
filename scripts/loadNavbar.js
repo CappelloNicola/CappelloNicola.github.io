917a7901624d8993b301418a1dfc47ab14511cd0
CappelloNicola <cappellonicola04@gmail.com>
2022-12-30 13:57:03 +0100

    Projects page prepared
    loadNavbar.js updated to change the currentPage text

--- scripts/loadNavbar.js
+++ scripts/loadNavbar.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-    $('#loadNavbar').load('../pages/navbar.html');
+    $('#loadNavbar').load('../pages/navbar.html', function(responseTxt, statusTxt, xhr){
+        var current_title = $("title").text();
+        if(current_title === "Nicola Cappello - Portfolio"){
+            current_title = "Home";
+        }
+        $("#currentPageName").text(current_title);
+    });
 });
